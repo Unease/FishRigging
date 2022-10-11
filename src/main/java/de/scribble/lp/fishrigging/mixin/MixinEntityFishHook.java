6f7e7dfaf1faa0e80e19f8cafcb70ea6bced8f7d
Scribble <scribble@minecrafttas.com>
2022-10-11 21:21:30 +0200

    Finished rework

--- src/main/java/de/scribble/lp/fishrigging/mixin/MixinEntityFishHook.java
+++ src/main/java/de/scribble/lp/fishrigging/mixin/MixinEntityFishHook.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Random;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -47,16 +48,26 @@ public abstract class MixinEntityFishHook {
     @Redirect(method = "catchingFish", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;getInt(Ljava/util/Random;II)I", ordinal = 1))
     private int fastestFishApproachRedirect(Random rand, int min, int max) {
     	if(FishManipEvents.fishrigger.isActive()) {
-    		return 20;
+    		return 21;
     	}else {
     		return MathHelper.getInt(rand, min, max);
     	}
     }
     
+    @Shadow
+    private int lureSpeed;
+    
     @Redirect(method = "catchingFish", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;getInt(Ljava/util/Random;II)I", ordinal = 2))
     private int fastestFishAppearRedirect(Random rand, int min, int max) {
     	if(FishManipEvents.fishrigger.isActive()) {
-    		return 100;
+    		switch(lureSpeed) {
+    		case 2:
+    			return 201;
+    		case 3:
+    			return 301;
+    		default:
+    			return 101;
+    		}
     	}else {
     		return MathHelper.getInt(rand, min, max);
     	}
