a291c2f697d967f8bc0f0c5503b9d5006774f792
Noah Tung <uneasepurse37@gmail.com>
2026-04-10 15:18:55 -0700

    Modified redirects to use WrapOperation

--- src/main/java/com/minecrafttas/fishrigging/mixin/MixinEntityFishHook.java
+++ src/main/java/com/minecrafttas/fishrigging/mixin/MixinEntityFishHook.java
@@ -5,12 +5,12 @@ import java.util.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.minecrafttas.fishrigging.FishRigging;
 
 import net.minecraft.entity.projectile.EntityFishHook;
-import net.minecraft.util.math.MathHelper;
 
 @Mixin(EntityFishHook.class)
 public class MixinEntityFishHook {
@@ -23,54 +23,43 @@ public class MixinEntityFishHook {
 	private int f_16491628;
 	//# end
 	
-	@Redirect(method = "catchingFish", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F", ordinal = 0))
-	private float redirectRaining(Random rand) {
-		
+	@WrapOperation(method = "catchingFish", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F", ordinal = 0))
+	private float redirectRaining(Random rand, Operation<Float> original) {
 		if (FishRigging.isTASModLoaded) {
 			return 0;
 		}
-		
-		return rand.nextFloat();
+		return original.call(rand);
 	}
 	
-	@Redirect(method = "catchingFish", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F", ordinal = 1))
-	private float redirectOpenSky(Random rand) {
-		
+	@WrapOperation(method = "catchingFish", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F", ordinal = 1))
+	private float redirectOpenSky(Random rand, Operation<Float> original) {
 		if (FishRigging.isTASModLoaded) {
 			return 1;
 		}
-		
-		return rand.nextFloat();
+		return original.call(rand);
 	}
 	
-	@Redirect(method = "catchingFish", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;getInt(Ljava/util/Random;II)I", ordinal = 1))
-	private int redirectMinWait(Random rand, int min, int max) {
-		
+	@WrapOperation(method = "catchingFish", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;getInt(Ljava/util/Random;II)I", ordinal = 1))
+	private int redirectMinWait(Random rand, int min, int max, Operation<Integer> original) {
 		if (FishRigging.isTASModLoaded) {
 			return 20;
 		}
-		return MathHelper.getInt(rand, 20, 80);
+		return original.call(rand);
 	}
 	
-	@Redirect(method = "catchingFish", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;getInt(Ljava/util/Random;II)I", ordinal = 2))
-	private int redirectFishApproach(Random rand, int min, int max) {
-		
+	@WrapOperation(method = "catchingFish", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;getInt(Ljava/util/Random;II)I", ordinal = 2))
+	private int redirectFishApproach(Random rand, int min, int max, Operation<Integer> original) {
 		//# 1.12.2
 //$$		int lureSpeed = this.lureSpeed;
 		//# 1.11.2
 		int lureSpeed = this.f_16491628;
 		//# end
-		
 		if (FishRigging.isTASModLoaded) {
-			
 			if (lureSpeed >= 1) {
 				return lureSpeed * 100 + 1;
 			}
-			
 			return 100;
-			
 		}
-
-		return MathHelper.getInt(rand, 100, 600);
+		return original.call(rand);
 	}
 }
